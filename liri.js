ed9a6dfd3544e4bc6f9e82c617bb5ebcb2d80e2a
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2019-02-05 12:23:00 -0500

    almost there

--- liri.js
+++ liri.js
@@ -3,111 +3,165 @@ var fs = require("fs");
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
-var moment = require("moment")
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
- 
-switch (process.argv[2]){
-    case "concert-this":
-        var artist = process.argv.slice(3).join(" ");
+var readArr = [];
 
-        axios
-            .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-            .then(function(res){
+function concertThis(){
+    var posArt = process.argv.slice(3).join(" ");
+    var artist = "";
 
-                var response = res.data;
+    if (process.argv[3] != undefined){
+        artist = posArt;
+        run();
+    }else if (readArr[0] === "concert-this"){
+        artist = readArr[1];
+        run();
+    }else{
+        console.log("Please let me know who you would like to see.");
+    };
 
-                for (i = 0; i < response.length; i++ ){
+    function run() {
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(res){
+            var response = res.data;
+            for (i = 0; i < response.length; i++ ){
 
-                    var date = moment(response[i].datetime, moment.HTML5_FMT.DATETIME_LOCAL).format("MM/DD/YYYY")
+                var date = moment(response[i].datetime, moment.HTML5_FMT.DATETIME_LOCAL).format("MM/DD/YYYY");
+                console.log("--------------------------------------------------------------");
+                console.log("Venue: " + response[i].venue.name);
+                console.log("City: " + response[i].venue.city);
+                console.log("Country: " + response[i].venue.country);
+                console.log("When: " + date);
+                
+                if (i == (response.length -1)) {
                     console.log("--------------------------------------------------------------");
-                    console.log("Venue: " + response[i].venue.name);
-                    console.log("City: " + response[i].venue.city);
-                    console.log("Country: " + response[i].venue.country);
-                    console.log("When: " + date);
-                    
-                    if (i == (response.length -1)) {
-                        console.log("--------------------------------------------------------------");
-                        console.log("The End!")
-
-                    }
+                    console.log("The End!");
 
                 };
+            };
+        }).catch(function(err){
+            console.log(err);
+        });
+    };
+};
 
-            }).catch(function(err){
+function spotifyThisSong(){
+    var possible = process.argv.slice(3).join(" ");
+    var song = "The Sign";
 
-                console.log(err)
+    if (process.argv[3] != undefined){
+        song = possible;
+    }else if (readArr != []){
+        song = readArr[0];
+    };
+        
+    spotify.search({
+        type: "track",
+        query: song,
+    }, function(err,data){
 
-            });
+        if (err){
+            console.log("Error occurred: " + err);
+        };
 
+        if (song != "The Sign"){
+            for (j = 0; j < data.tracks.items.length; j++){
+                console.log("-------------------------------------------------");
+                console.log("Artist: " + data.tracks.items[j].artists[0].name);
+                console.log("Song: " + data.tracks.items[j].name);
+                console.log("Preview Song Link: " + data.tracks.items[j].preview_url);
+                console.log("Album: " + data.tracks.items[j].album.name);
+            };
+            if (j = (data.tracks.items.length -1)){
+                console.log("-------------------------------------------------");
+                console.log("Thats all I've got!");
+            };
+        }else{ 
+                console.log("-------------------------------------------------");
+                console.log("Artist: " + data.tracks.items[9].artists[0].name);
+                console.log("Song: " + data.tracks.items[9].name);
+                console.log("Preview Song Link: " + data.tracks.items[9].preview_url);
+                console.log("Album: " + data.tracks.items[9].album.name);
+                console.log("-------------------------------------------------");
+        };
+    });
+};
+
+function movieThis(){
+    var posMovie = process.argv.slice(3).join("+");
+    var movie = "mr nobody";
+    if (process.argv[3] != undefined){
+        movie = posMovie;
+    }else if (readArr != []){
+        movie = readArr[0];
+    };
+
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&plot=short&apikey=trilogy").then(function(res){
+        var response = res.data;
+            
+        console.log("--------------------------------------------------------------");
+        console.log("Title: " + response.Title);
+        console.log("Year: " + response.Year);
+        console.log("IMDB Rating: " + response.Ratings[0].value);
+        console.log("Rotten Tomatoes Rating: " + response.Ratings[1].value);
+        console.log("Country Produced in: " + response.Country);
+        console.log("Language: " + response.Language);
+        console.log("Plot: " + response.Plot);
+        console.log("Cast: " + response.Actors);
+        console.log("--------------------------------------------------------------");
+        console.log("The End!");
+            
+    }).catch(function(err){
+        console.log(err);   
+    });
+}
+
+function readThis(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        };
+
+        var dataArr = data.split(",");
+        
+        if(dataArr[0] === "spotify-this-song"){
+            readArr.push(dataArr[1]);
+            spotifyThisSong();
+        };
+        if(dataArr[0] === "movie-this"){
+            readArr.push(dataArr[1]);
+            movieThis();
+        };
+        if(dataArr[0] === "concert-this"){
+            readArr.push(dataArr[0]);
+            readArr.push(dataArr[1]);
+            concertThis();
+        };
+      
+      });
+};
+ 
+
+switch (process.argv[2]){
+    case "concert-this":
+        concertThis();
         break;
 
     case "spotify-this-song":
-        var possible = process.argv.slice(3).join(" ")
-        var song = "The Sign";
-        if (process.argv[3] != undefined){
-            song = possible;
-        };
-
-        spotify.search({
-            type: "track",
-            query: song,
-        }, function(err,data){
-
-            if (err){
-                console.log("Error occurred: " + err);
-            };
-
-            
-            if (song != "The Sign"){
-                for (j = 0; j < data.tracks.items.length; j++){
-                    console.log("-------------------------------------------------");
-                    console.log("Artist: " + data.tracks.items[j].artists[0].name);
-                    console.log("Song: " + data.tracks.items[j].name);
-                    console.log("Preview Song Link: " + data.tracks.items[j].preview_url);
-                    console.log("Album: " + data.tracks.items[j].album.name);
-                };
-                if (j = (data.tracks.items.length -1)){
-                    console.log("-------------------------------------------------");
-                    console.log("Thats all I've got!");
-                };
-            }
-
-            for (i = 0; i < data.tracks.items.length; i++){
-
-                if (data.tracks.items[i].artist[0].name == "Ace of Base"){  
-                        var num = i
-                        console.log("-------------------------------------------------");
-                        console.log("Artist: " + data.tracks.items[num].artists[0].name);
-                        console.log("Song: " + data.tracks.items[num].name);
-                        console.log("Preview Song Link: " + data.tracks.items[num].preview_url);
-                        console.log("Album: " + data.tracks.items[num].album.name);
-                };
-
-                if (i = (data.tracks.items.length -1)){
-                    console.log("-------------------------------------------------");
-                    console.log("Thats all I've got!");
-                };
-            };
-
-        });
-
-        console.log();
+        spotifyThisSong();
         break;
 
     case "movie-this":
-        
-    
-        console.log();
+        movieThis();
         break; 
         
     case "do-what-it-says":
-        
-    
-        console.log();
+        readThis();
         break;
 
     default:
-        console.log();
+        console.log("What can I help you with today?");
         break;
 
-}
+};
