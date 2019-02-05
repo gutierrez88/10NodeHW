92bea71b6316260f91da8eb4ba35834fdb77630f
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2019-02-04 20:53:12 -0500

    setup

---
+++ liri.js
@@ -0,0 +1,31 @@
+js 
+require("dotenv").config();
+
+js 
+var keys = require("./keys.js");
+
+js 
+var spotify = new Spotify(keys.spotify);
+
+switch (process.argv[2]){
+    case "concer-this":
+        console.log()
+        break;
+
+    case "concer-this":
+        console.log()
+        break;
+
+    case "concer-this":
+        console.log()
+        break; 
+        
+    case "concer-this":
+        console.log()
+        break;
+
+    default:
+        console.log()
+        break;
+        
+}
